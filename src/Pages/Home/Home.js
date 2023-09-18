540476e0495c0f73e0e8cf172fd60ee78a30ac79
andreonofre <alordandre@gmail.com>
2023-09-17 23:08:16 -0300

    lista de filmes

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,32 +1,50 @@
-import React from 'react'
-import { useEffect, useState } from 'react';
-import Api from '../../services/Api';
+import React from "react";
+import { useEffect, useState } from "react";
+import Api from "../../services/Api";
+import { Link } from "react-router-dom";
+import "./home.css";
 
 //https://api.themoviedb.org/3/movie/now_playing?api_key=0ac9705aaf63159f0dd6dee108a606ae&language=pt-BR
 
 const Home = () => {
   const [filmes, setFilmes] = useState([]);
 
-
   //Quando carregar a minha página, vai fazer a requisição a API
   useEffect(() => {
-    async function loadFilmes () {
+    async function loadFilmes() {
+      //Response está o retorno dos daddos da minha API, Status 200 = OK
       const response = await Api.get("/movie/now_playing", {
         params: {
           api_key: "0ac9705aaf63159f0dd6dee108a606ae",
           language: "pt-BR",
           page: 1,
-        }
+        },
       });
-      console.log(response.data.results[0]);
-    };
+      // console.log(response.data.results[0]);
+      setFilmes(response.data.results);
+    }
     loadFilmes();
-  },[]);
+  }, []);
 
+  console.log("Filmes: ", filmes);
 
   return (
-    <div>
-        <h1>Bem vindo a página HOME</h1>
+    <div className="Container">
+      <div className="listaFilmes">
+
+        {filmes.map((filme) => {
+          return (
+            <article key={ filme.id }>
+              <strong className="title"> { filme.title } </strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+
+              {/* Vai levar para a rota de filmes com pelo ID */}
+              <Link to={`/filme/${filme.id}`}>Acessar</Link> 
+              <span> { filme.vote_average } </span>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 };
