1408e9bcd406575c9b86cfd4b8a838cecacdb389
andreonofre <alordandre@gmail.com>
2023-09-21 20:42:23 -0300

    redirecionamento trailer

--- src/Pages/Filme/Filme.js
+++ src/Pages/Filme/Filme.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import Api from '../../services/Api';
 import "./detalhes.css";
 
@@ -8,6 +8,7 @@ import "./detalhes.css";
 const Filme = () => {
   //pegando o meu ID ao clicar no link que direciona pra essa rota
   const { id } = useParams(); 
+  const navigate = useNavigate();
   const [filme, setFilme] = useState({});
   const [load, setLoad] = useState(true);
 
@@ -25,6 +26,9 @@ const Filme = () => {
       })
       .catch(() => {
         console.log("Filme não encontrado")
+        // redirecionar para a pagina HOME
+        navigate("/", { replace: true });
+        return;
       })
     };
 
@@ -35,7 +39,7 @@ const Filme = () => {
     return () => {
       console.log("Componente desmontou.")
     };
-  },[]);
+  },[navigate, id]);
 
 
 
@@ -54,12 +58,12 @@ const Filme = () => {
        <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
        <h3>Sinopse</h3>
        <span> {filme.overview} </span>
-       <strong>Avaliação: {filme.vote_average.toFixed(2)} /10</strong>
+       <strong>Avaliação: {filme.vote_average.toFixed(1)} /10</strong>
 
        <div className='area-buttons'>
         <button>Salvar</button>
         <button>
-          <a href="#">
+          <a target='_blank' rel="external" href={`https://youtube.com/results?search_query=${filme.title} trailer`}>
             Trailler
           </a>
           </button>
